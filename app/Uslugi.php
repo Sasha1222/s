b8e8e7ba981208adffc5d6fa44ec17953fce3de1
Sasha <dschungelcamp123@gmail.com>
2019-05-23 12:18:41 +0300

    update

---
+++ app/Uslugi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Uslugi extends Model
+{
+    protected $table = 'uslugi';
+    public $primaryKey = 'id'; 
+}
---
+++ resources/views/uslugi/index.blade.php
@@ -0,0 +1,83 @@
+    
+    @extends('main')
+
+@section('content')
+  <main role="main">
+   <div class="size-container">
+    <div class="breadcrumbs" itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+        <a href="#" itemprop="url"><span itemprop="title">Главная</span></a>
+        <span class="separator">/</span>
+        <span itemprop="title">Новости</span>
+      </div>
+ <div class="news">
+       <div class="news-header">
+          <h1>Новости</h1>
+        </div>
+            <div class="news-container">
+                            
+                    <div class="item">
+                  <a href="/uslugi/25-maya-protantsy-kontsert-kids/" class="item-container ">
+                                                            <img src="http://tancy.pro/upload/resize_cache/iblock/b29/555_555_2/b29f9fe41a975b074c3370281b7d6dab.jpg" alt="25 мая - PROТАНЦЫ.Концерт kids">
+                                                <div class="desc">
+                        <div class="desc-container">
+                           <div class="name">25 мая - PROТАНЦЫ.Концерт kids</div>
+                           <div class="intro">20.05.2019 23:26</div>
+                        </div> <!-- eo .desc-container -->
+                     </div> <!-- eo .desc -->
+                  </a>
+        </div> <!-- eo .item -->
+              <div class="item">
+                  <a href="/uslugi/25-maya-protantsy-kontsert-nazad-v-budushchee/" class="item-container ">
+                                                            <img src="http://tancy.pro//upload/resize_cache/iblock/8be/555_555_2/8be35f499f6c3fae92e29a32c4c5f058.jpg" alt="25 мая - PROТАНЦЫ.Концерт &quot;Назад в будущее&quot;">
+                                                <div class="desc">
+                        <div class="desc-container">
+                           <div class="name">25 мая - PROТАНЦЫ.Концерт "Назад в будущее"</div>
+                           <div class="intro">17.05.2019 18:14</div>
+                        </div> <!-- eo .desc-container -->
+                     </div> <!-- eo .desc -->
+                  </a>
+        </div> <!-- eo .item -->
+              <div class="item">
+                  <a href="/uslugi/prodlyenka/" class="item-container ">
+                                                            <img src="http://tancy.pro//upload/resize_cache/iblock/e7d/555_555_2/e7d482942d9ea7ad202a9cf56f56ce08.jpg" alt="Летняя программа PROДЛЁНКА для детей 7-13 лет!">
+                                                <div class="desc">
+                        <div class="desc-container">
+                           <div class="name">Летняя программа PROДЛЁНКА для детей 7-13 лет!</div>
+                           <div class="intro">30.04.2019 19:21</div>
+                        </div> <!-- eo .desc-container -->
+                     </div> <!-- eo .desc -->
+                  </a>
+        </div> <!-- eo .item -->
+              <div class="item">
+                  <a href="/uslugi/abonementy-leto/" class="item-container ">
+                                                            <img src="http://tancy.pro//upload/resize_cache/iblock/da4/555_555_2/da48ee9194b386ad4eee30a9d4098810.png" alt="Абонементы Лето">
+                                                <div class="desc">
+                        <div class="desc-container">
+                           <div class="name">Абонементы Лето</div>
+                           <div class="intro">30.04.2019 19:11</div>
+                        </div> <!-- eo .desc-container -->
+                     </div> <!-- eo .desc -->
+                  </a>
+        </div> <!-- eo .item -->
+            
+         <div style="display:none" id="pager"><div class="modern-page-navigation">
+   <span class="modern-page-title">Страницы:</span>
+         <a class="modern-page-previous" href="/uslugi/?ajax=Y&amp;PAGEN_1=2">Пред.</a>
+      <a href="/uslugi/?ajax=Y" class="modern-page-first">1</a>
+      <a href="/uslugi/?ajax=Y&amp;PAGEN_1=2" class="">2</a>
+      <span class="modern-page-current">3</span>
+      <a href="/uslugi/?ajax=Y&amp;PAGEN_1=4" class="">4</a>
+      <a href="/uslugi/?ajax=Y&amp;PAGEN_1=5" class="">5</a>
+      <a class="modern-page-dots" href="/uslugi/?ajax=Y&amp;PAGEN_1=18">...</a>
+      <a href="/uslugi/?ajax=Y&amp;PAGEN_1=31">31</a>
+      <a class="modern-page-next" href="/uslugi/?ajax=Y&amp;PAGEN_1=4">След.</a>
+</div></div><div class="item news-more" style="top:auto;"><a class="btn blue-bordered btn-xxl">Ещe</a></div></div></div>
+
+   </div>
+</main>
+@endsection
+
+ @section('javascript')
+  
+ @endsection
+  
